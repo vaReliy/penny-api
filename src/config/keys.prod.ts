892836bf2a087895476390844114629519b06b4c
Valeriy Garbuzov <valeriy@i.ua>
2021-01-06 17:04:18 +0200

    add base config, update prettier

---
+++ src/config/keys.prod.ts
@@ -0,0 +1,6 @@
+export const KEYS = {
+  SERVER_PORT: process.env.SERVER_PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+import { KEYS } from './config/keys'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(KEYS.SERVER_PORT)
 }
-bootstrap();
+bootstrap()
